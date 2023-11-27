c23a75019829b86385c376615ae126ca8035e7f3
Conner-dev <56259288+Conner-dev@users.noreply.github.com>
2023-11-27 11:27:54 +0100

    Update bookingForm.jsx

--- BertsBoerenCamping/src/components/bookingForm.jsx
+++ BertsBoerenCamping/src/components/bookingForm.jsx
@@ -1,25 +1,71 @@
-import react from 'react';
+import React, { useState } from 'react';
 
 function BookingForm() {
-    return (
-<form>
-    <label>
-        Naam:
-        <input type="text" name='name' />
-    </label>
-    <label>
+  const [formData, setFormData] = useState({
+    voornaam: '',
+    achternaam: '',
+    email: '',
+    telefoonnummer: '',
+    voorkeuren: '',
+    datum: '', 
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://your-backend-api-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      // Handle response if needed
+      console.log('Response:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Voornaam:
+        <input type="text" name="voornaam" value={formData.voornaam} onChange={handleChange} />
+      </label>
+      <label>
+        Achternaam:
+        <input type="text" name="achternaam" value={formData.achternaam} onChange={handleChange} />
+      </label>
+      <label>
         Email:
-        <input type="email" name='email' />
-    </label>
-    <label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </label>
+      <label>
+        Telefoonnummer:
+        <input type="tel" name="telefoonnummer" value={formData.telefoonnummer} onChange={handleChange} />
+      </label>
+      <label>
+        Voorkeuren:
+        <input type="text" name="voorkeuren" value={formData.voorkeuren} onChange={handleChange} />
+      </label>
+      <label>
         Datum:
-        <input type="date" name='datum' />
-    </label>
-    <button>
-        Klik hier om te boeken!
-    </button>
-</form>
-    )
+        <input type="date" name="datum" value={formData.datum} onChange={handleChange} />
+      </label>
+      <button type="submit">Klik hier om te boeken!</button>
+    </form>
+  );
 }
 
 export default BookingForm;
