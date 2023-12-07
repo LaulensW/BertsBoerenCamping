314d538dd0e22e855fe3a7999b555f95fc56328a
Niek218 <niek.wolbrink@hotmail.com>
2023-12-07 19:38:05 +0100

    Login + registreren (hashed)
    - npm bcrypt (voor het hashen van wachtwoorden)
    - Baliemedewerker mogelijkheid om te registreren, Wachtwoord wat je invoerd wordt gehashed en opgeslagen in database
    - Inloggen mogelijk door idMedewerker + wachtwoord in te voeren. (dit wachtwoord wordt uit database gehaald en geunhashed).

--- BertsBoerenCamping/src/components/LoginForm.jsx
+++ BertsBoerenCamping/src/components/LoginForm.jsx
@@ -4,32 +4,40 @@ const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+const handleLogin = async (e) => {
+  e.preventDefault();
+  setLoading(true);
 
-    try {
-      const response = await fetch('http://localhost:3001/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+  try {
+    const response = await fetch('http://localhost:3001/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
 
-      if (response.ok) {
-        // Login successful
-        setError('');
-        // Redirect or perform any other action
-      } else {
-        const data = await response.json();
-        setError(data.error);
-      }
-    } catch (error) {
-      console.error('Error logging in:', error);
-      setError('Internal Server Error');
+    console.log('Response status:', response.status);
+
+    if (response.status === 200) {
+      console.log('Login successful');
+      setError('');
+      setUsername('');
+      setPassword('');
+      // Redirect or perform any other action
+    } else {
+      const data = await response.json();
+      setError(data.error || 'Login failed');
     }
-  };
+  } catch (error) {
+    console.error('Error logging in:', error);
+    setError('Failed to fetch. Check your network connection.');
+  } finally {
+    setLoading(false);
+  }
+};
 
   return (
     <form onSubmit={handleLogin}>
@@ -51,8 +59,10 @@ const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      {error && <div>{error}</div>}
-      <button type="submit">Login</button>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <button type="submit" disabled={loading}>
+        {loading ? 'Logging in...' : 'Login'}
+      </button>
     </form>
   );
 };
