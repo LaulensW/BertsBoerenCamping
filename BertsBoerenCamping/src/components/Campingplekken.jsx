ab12404623dddaed6999621461b3e080a4bb3bf2
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-13 11:14:57 +0100

    Meer campingplekken toegevoegd + scrollbar

--- BertsBoerenCamping/src/components/Campingplekken.jsx
+++ BertsBoerenCamping/src/components/Campingplekken.jsx
@@ -8,12 +8,43 @@ export default function Campingplekken() {
     <>
         <h3 className="onze-campingplekken">ONZE CAMPINGPLEKKEN</h3>
         <p3 className="onze-campingplekken-cursive">onze campingplekken</p3>
-        < IndividueleCampingplek 
+        <div className="campingplekken-total">
+            < IndividueleCampingplek 
             name="CAMPINGPLEK A"
-        /> 
-        < IndividueleCampingplek 
+            price="€100"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            /> 
+            < IndividueleCampingplek 
             name="CAMPINGPLEK B"
-        /> 
+            price="€95"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            /> 
+            < IndividueleCampingplek 
+            name="CAMPINGPLEK C"
+            price="€97"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            /> 
+            < IndividueleCampingplek 
+            name="CAMPINGPLEK D"
+            price="€96"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            /> 
+            < IndividueleCampingplek 
+            name="CAMPINGPLEK E"
+            price="€103"
+            description="Nulla aliquam tempor odio, eget viverra nisi posuere sit."
+            /> 
+            < IndividueleCampingplek 
+            name="CAMPINGPLEK F"
+            price="€99"
+            description="Nulla aliquam tempor odio, eget viverra nisi posuere sit."
+            /> 
+            < IndividueleCampingplek 
+            name="CAMPINGPLEK G"
+            price="€107"
+            description="Nulla aliquam tempor odio, eget viverra nisi posuere sit."
+            /> 
+        </div>
     </>
     );
 };
--- BertsBoerenCamping/src/components/IndividueleCampingplek.jsx
+++ BertsBoerenCamping/src/components/IndividueleCampingplek.jsx
@@ -6,6 +6,8 @@ export default function IndividueleCampingplek(props) {
         <div className="campeerplek">
             <img src="../images/Filler.png" alt="Filler" className="filler-photo"/>
             <h4 className="campingplek-name">{props.name}</h4>
+            <p3 className="campingplek-price">{props.price}</p3>
+            <p3 className="campingplek-description">{props.description}</p3>
         </div>
     );
 };
