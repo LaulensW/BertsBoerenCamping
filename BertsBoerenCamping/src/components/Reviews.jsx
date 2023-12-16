6e3e07358d6874eb351bc37dfc810c108e200e95
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-16 16:26:09 +0100

    Review scroll gefixt + temporary rating toegevoegd

--- BertsBoerenCamping/src/components/Reviews.jsx
+++ BertsBoerenCamping/src/components/Reviews.jsx
@@ -7,23 +7,27 @@ export default function Reviews() {
             <h3 className="reviews">REVIEWS</h3>
             <p3 className="reviews-cursive">reviews</p3>
             <div className="reviews-total">
-                {renderReviews("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
-                {renderReviews("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
-                {renderReviews("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
-                {renderReviews("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
-                {renderReviews("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
+                {renderReview("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+                {renderReview("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
+                {renderReview("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
+                {renderReview("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
+                {renderReview("Review Title", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.", "5")}
             </div>
         </>
     );
-}
+};
 
-function renderReviews(title, author, content, rating) {
+function renderReview(title, author, content, rating) {
     return (
        <div className="reviewcard" key={title}>
-            <h4 className="review-title">{title}</h4>
-            <p4 className="review-author">{author}</p4>
-            <p3 className="review-content">{content}</p3>
-            <h3 className="review-rating">{rating}</h3>
+            <div className="review-info">
+                <h4 className="review-title">{title}</h4>
+                <p4 className="review-author">{author}</p4>
+                <p3 className="review-content">{content}</p3>
+                <h3 className="review-rating">{rating}
+                <img src="../images/rating-5.png" className="rating-image"/></h3>
+            </div>
        </div>
     );
-}
+};
+
