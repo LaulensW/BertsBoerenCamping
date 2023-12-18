69813e09d434fb9372a3dc52f62458d5a50c979d
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-18 13:53:14 +0100

    Headings toegevoegd en titels gestyled

--- BertsBoerenCamping/src/components/Footer.jsx
+++ BertsBoerenCamping/src/components/Footer.jsx
@@ -7,9 +7,22 @@ const Footer = () => {
         <div className="footer-container">
             <div className="footer">
                 <div className="footer-heading-1">
-                    <p className="heading-boeken">BOEKEN</p>
-                    <p class="heading-boeken-cursive">boeken</p>
+                    <h6 className="heading">BOEKEN</h6>
+                    <p className="heading-cursive">boeken</p>
                     <a href="#">Boeken</a>
+                </div>
+                <div className="footer-heading-2">
+                    <h6 className="heading">OVER ONS</h6>
+                    <p className="heading-cursive">over ons</p>
+                    <a href="#">Over ons</a>
+                </div>
+                <div className="footer-heading-3">
+                    <h6 className="heading">CONTACT</h6>
+                    <p className="heading-cursive">contact</p>
+                    <a href="#">info@example.com</a>
+                    <p className="'heading-email"></p>
+                    <input type="email" placeholder="Email" id="footer-email"/>
+                    <input type="submit" placholder="Verzenden" id="footer-email-btn"/>
                 </div>
             </div>
         </div>
