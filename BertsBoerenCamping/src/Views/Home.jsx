2533e2ee48266e0bb844e1b8ea1474d93f09cd98
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-18 11:37:14 +0100

    Footer aangemaakt en import in HOME

--- BertsBoerenCamping/src/Views/Home.jsx
+++ BertsBoerenCamping/src/Views/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
+    <>
     <Layout>
       <h2>Home Page</h2>
       {/* Home-specific content */}
     </Layout>
+    < Footer />
+    </>
   );
 };
 
---
+++ BertsBoerenCamping/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <>
+        <div className="footer-container">
+            <div className="footer">
+                <div className="footer-heading-1">
+                    <p className="heading-boeken">BOEKEN</p>
+                    <p class="heading-boeken-cursive">boeken</p>
+                    <a href="#">Boeken</a>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Footer;
