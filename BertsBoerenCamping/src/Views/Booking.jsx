57e8ffea325570d9e9c2bcd836e983aa267e6749
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-20 10:18:21 +0100

    Booking banner file toegevoegd en svg toegevoegd voor wave

--- BertsBoerenCamping/src/Views/Booking.jsx
+++ BertsBoerenCamping/src/Views/Booking.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import BookingForm from '../components/bookingForm';
 import Layout from '../components/Layout';
+import BookingBannerEnUitleg from '../components/BookingBannerEnUitleg';
 
 const Booking = () => {
   return (
     <div>
       <Layout> 
       <h2></h2>
+      <BookingBannerEnUitleg />
       <BookingForm />
       </Layout> 
+      
     </div>
   );
 };
---
+++ BertsBoerenCamping/src/components/BookingBannerEnUitleg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './BookingBannerEnUitleg.css';
+
+const Banner = () => {
+    return(
+        <>
+        <img src="../images/Booking_Banner.png" className="booking-banner"/>
+        </>
+    );
+};
+export default Banner;
