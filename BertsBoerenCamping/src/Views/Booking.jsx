4d37e79a9e1ed1f09511e38f205ac0670af7f088
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-20 12:17:32 +0100

    Titels veranderd en svg bij plattegrond toegevoegd

--- BertsBoerenCamping/src/Views/Booking.jsx
+++ BertsBoerenCamping/src/Views/Booking.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BookingForm from '../components/bookingForm';
 import Layout from '../components/Layout';
 import BookingBannerEnUitleg from '../components/BookingBannerEnUitleg';
+import PlattegrondWave from '../assets/booking-plattegrond-wave.svg';
 
 const Booking = () => {
   return (
@@ -9,7 +10,8 @@ const Booking = () => {
       <Layout> 
       <h2></h2>
       <BookingBannerEnUitleg />
-      <br/>
+      <br/> 
+      <img src={PlattegrondWave} width="100%"/>
       <BookingForm />
       </Layout> 
       
