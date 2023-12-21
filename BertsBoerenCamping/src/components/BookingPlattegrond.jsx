3d46c4ae3722bfd696b8d835d564125d5402dfeb
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2023-12-21 18:09:47 +0100

    2 campingplekken naar 1 campingplek

--- BertsBoerenCamping/src/components/BookingPlattegrond.jsx
+++ BertsBoerenCamping/src/components/BookingPlattegrond.jsx
@@ -9,12 +9,11 @@ const BookingPlattegrond = () => {
         <div className="booking-plattegrond">
             <div className="rectangle">
                 <Link to="/about"><div className="rectangle-plekken" style={{width:"287px", height:"134px", margin: "32px 0 0 223px"}}>A</div></Link>
-                <Link to="/about"><div className="rectangle-plekken" style={{width:"287px", height:"134px", margin: "265px 0 0 223px"}}>B</div></Link> 
             </div>
         </div>      
 
         <div className="booking-legenda">
-            <div className="rectangle-kegenda">
+            <div className="rectangle-legenda">
 
             </div>
         </div>    
