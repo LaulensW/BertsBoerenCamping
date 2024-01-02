4e9648e3e8fdfec22dfaca8549770c75921323ed
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-02 15:46:20 +0100

    About banner foto toegevoegd (kan veranderd worden), components gemaakt en ingevoegd in about view

--- BertsBoerenCamping/src/Views/About.jsx
+++ BertsBoerenCamping/src/Views/About.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Banner from '../components/AboutBanner';
 
 const About = () => {
   return (
-    <div>
+    <>
       <Layout>  
-      <h2>About Page</h2>
-      {/* Content specific to the about page */}
-      </Layout>
-    </div>
+      <h2>About Page</h2>      
+ 
+      </Layout>     
+      <Banner/>
+    </>
   );
 };
 
---
+++ BertsBoerenCamping/src/components/AboutBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutBanner.css';
+import BannerWave from '../assets/about-banner-wave.svg';
+
+const AboutBanner = () => {
+    return(  
+        <>
+        <div className="about-banner">
+            <img src="../images/About_Banner.png" alt="Banner" className="banner-background"/>
+            
+        </div>
+        </>
+    );
+};
+
+export default AboutBanner;
