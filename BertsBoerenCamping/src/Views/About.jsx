ea8fd50e02fa7bdd9e1833416252ee2c38519f18
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-03 17:18:43 +0100

    ons team wave en ons team component aangemaakt

--- BertsBoerenCamping/src/Views/About.jsx
+++ BertsBoerenCamping/src/Views/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Banner from '../components/AboutBanner';
+import OnsTeam from '../components/AboutTeam';
 
 const About = () => {
   return (
     <>
       <Layout>      
-       <Banner/>
-      </Layout>     
+        < Banner />
+      </Layout>  
+      < OnsTeam />      
     </>
   );
 };
---
+++ BertsBoerenCamping/src/components/AboutTeam.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutTeam.css';
+import Wave from '../assets/team-wave.svg';
+
+const OnsTeam = () => {
+    return(  
+        <>
+        <img src={Wave} alt="ons-team-wave" className="ons-team-wave"/>
+        </>
+    );
+};
+
+export default OnsTeam;
