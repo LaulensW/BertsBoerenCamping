212fb362233429ebf40d52c09aea3a4fdf528b0f
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-05 15:54:43 +0100

    ons team afgemaakt en props tegeveoegd

--- BertsBoerenCamping/src/components/AboutTeam.jsx
+++ BertsBoerenCamping/src/components/AboutTeam.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './AboutTeam.css';
 import Wave from '../assets/team-wave.svg';
 
-const OnsTeam = () => {
+export default function OnsTeam() {
     return(  
         <>
         <img src={Wave} alt="ons-team-wave" className="ons-team-wave"/> 
@@ -11,15 +11,27 @@ const OnsTeam = () => {
                 <h className="ons-team-title">ONS TEAM</h>
                 <p className="ons-team-title-cursive">ons team</p> 
             </div>
-            <img src="../images/Filler.png" alt="profile" className="ons-team-profile-photo"/>
-            <img src="../images/Filler.png" alt="profile" className="ons-team-profile-photo"/>
-            <img src="../images/Filler.png" alt="profile" className="ons-team-profile-photo"/>
-            <img src="../images/Filler.png" alt="profile" className="ons-team-profile-photo"/>
-            <img src="../images/Filler.png" alt="profile" className="ons-team-profile-photo"/>
+            <div className="team-total">
+                {renderProfiel('Filler.png', "Sakura", "Manager")}
+                {renderProfiel('Filler.png', "Chaewon", "Manager")}
+                {renderProfiel('Filler.png', "Yunjin", "Manager")}
+                {renderProfiel('Filler.png', "Kazuha", "Manager")}
+                {renderProfiel('Filler.png', "Eunchae", "CEO")}
+            </div>
         </div>
         
         </>
     );
 };
 
-export default OnsTeam;
+function renderProfiel(photo, name, position){
+    return(
+    <div className="team-profile" key={name}>
+         <div className="team-info">
+            <img src={`/images/${photo}`} alt="profiel" className="team-image" />
+            <p3 className="team-employee">{name}</p3>
+            <p3 className="team-position">{position}</p3>
+        </div>
+    </div>
+    );
+}
