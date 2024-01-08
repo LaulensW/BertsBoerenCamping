b66d322b4ed6eb3f4adf0b245f108be2f436ec50
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-08 20:51:56 +0100

    plattegrond layout beetje gefixt
    niet binnenin de plattegrond, maar de plattegrond als geheeld naast de legenda etc.

--- BertsBoerenCamping/src/components/BookingPlattegrond.jsx
+++ BertsBoerenCamping/src/components/BookingPlattegrond.jsx
@@ -5,19 +5,25 @@ import './BookingPlattegrond.css';
 const BookingPlattegrond = () => {
     return(
         <>
-        <h1 className="plattegrond-title">PLATTEGROND</h1>        
-        <h1 className="legenda-title" style={{margin:"0 0 0 1161px"}}>LEGENDA</h1>
-        <div className="booking-plattegrond">
-            <div className="rectangle">
-                <Link to="/about"><div className="rectangle-plekken" style={{width:"287px", height:"134px", margin: "32px 0 0 223px"}}>A</div></Link>
-            </div>                  
-        </div>
-        
-        <div className="rectangle-legenda">
+        <div className="plattegrond-en-legenda">
+            <div className="plattegrond-total">
+            <h1 className="plattegrond-title">PLATTEGROND</h1>        
+                <div className="booking-plattegrond">
+                    <div className="rectangle">
+                        <Link to="/about"><div className="rectangle-plekken" style={{width:"287px", height:"134px", margin: "32px 0 0 223px"}}>A</div></Link>
+                    </div>  
+                </div>
+            </div>
+
+            <div className="legenda-total">
+            <h1 className="legenda-title">LEGENDA</h1>
+                <div className="rectangle-legenda">
                     <div className="rectangle-legenda-inside" style={{backgroundColor: "#A0AD9E"}}>Ingang</div>
                     <div className="rectangle-legenda-inside"style={{backgroundColor: "#DCADA8"}}>Toiletten</div>
                     <div className="rectangle-legenda-inside" style={{backgroundColor: "#828A66"}}>Campingplekken</div>
-                </div>
+                </div>   
+            </div>  
+        </div>
         </>
     )
 };
