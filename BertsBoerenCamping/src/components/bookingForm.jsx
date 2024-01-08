30826e7d3c1908f29a05e669bbb22647b7b09fae
Conner-dev <56259288+Conner-dev@users.noreply.github.com>
2024-01-08 11:05:44 +0100

    Aanpassingen aan de boekingspagina
    Kleine aanpassingen

--- BertsBoerenCamping/src/components/bookingForm.jsx
+++ BertsBoerenCamping/src/components/bookingForm.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import './BookingForm.css';
-import { useNavigate } from 'react-router-dom';
+import Calender from './Calender';
+import './BookingForm.css'
 
-function BookingForm() {
+const BookingForm = () => {
   const [formData, setFormData] = useState({
     voornaam: '',
     tussenvoegsel: '',
     achternaam: '',
     email: '',
     telefoonnummer: '',
-    voorkeuren: '',
-    datum: null,
+    volwassenen: 1,
+    tieners: 0,
+    kinderen: 0,
+    accommodatieType: 'Selecteer',
   });
 
   const handleChange = (e) => {
@@ -23,117 +23,151 @@ function BookingForm() {
     }));
   };
 
-  const handleDateChange = (date) => {
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      datum: date,
+      [name]: value,
     }));
   };
 
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const [showErrorAlert, setShowErrorAlert] = useState(false);
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const requiredFields = ['voornaam', 'achternaam', 'email', 'telefoonnummer'].some(
-      (field) => formData[field].trim() === ''
-    );
-  
-    if (requiredFields) {
-      const nonRequiredFields = ['voorkeuren', 'tussenvoegsel'];
-      const nonRequiredFieldsFilled = nonRequiredFields.every(
-        (field) => formData[field].trim() !== ''
-      );
-  
-      if (!nonRequiredFieldsFilled) {
-        setShowErrorAlert(true);
-        setShowSuccessAlert(false);
-        return;
-      }
-    }
-
-    try {
-      const response = await fetch('http://localhost:3001/api/gast', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        setShowSuccessAlert(true);
-        setShowErrorAlert(false);
-        setTimeout(() => {
-          navigate('/');
-        }, 2000); 
-      } else {
-        console.error('Error:', response.statusText);
-      }
-
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-  const [dateRange, setDateRange] = useState([null, null]);
-  const [startDate, endDate] = dateRange;
-  
   return (
     <div className="booking-form-container">
-      <form className="booking-form" onSubmit={handleSubmit}>
-      <label>
-        Voornaam:
-        <input type="text" name="voornaam" value={formData.voornaam} onChange={handleChange} />
-      </label>
-      <label>
-        tussenvoegsel:
-        <input type="text" name="tussenvoegsel" value={formData.tussenvoegsel} onChange={handleChange} />
-      </label>
-      <label>
-        Achternaam:
-        <input type="text" name="achternaam" value={formData.achternaam} onChange={handleChange} />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-      </label>
-      <label>
-        Telefoonnummer:
-        <input type="tel" name="telefoonnummer" value={formData.telefoonnummer} onChange={handleChange} />
-      </label>
-      <label>
-        Voorkeuren:
-        <input type="text" name="voorkeuren" value={formData.voorkeuren} onChange={handleChange} />
-      </label>
-      {/* Datepicker */}
-      <div>
-        <label>Datum:</label>
-        <DatePicker
-          selectsRange={true}
-          startDate={startDate}
-          endDate={endDate}
-          onChange={(update) => {
-            setDateRange(update);
-          }}
-          isClearable={true}
-        />
+      <div className="info-box">
+        {/* Your information box content goes here */}
+        <h2>Additional Information</h2>
+        <h4>Ontdek de charme van het platteland en reserveer uw onvergetelijke campingervaring bij ons. Wij zijn enthousiast om u te verwelkomen op onze sfeervolle boerderij. Vul het onderstaande formulier in om uw verblijf te boeken en geniet binnenkort van de rust en natuurlijke schoonheid die onze camping te bieden heeft. Bij vragen staan wij altijd voor u klaar. <br />Tot snel op Berts Boeren Camping!</h4>
       </div>
-      {showErrorAlert && <div className="alert">All fields must be filled!</div>}
-      {showSuccessAlert && 
-      <div className="alert-success">    
-        <div className='inhoud'>
-        <h1>boeken</h1>
-        <img className="check" src="./images/check-circle-svgrepo-com.svg" alt="" />
-        <p>Uw boeking is gemaakt!</p>
-        </div>
-      </div>}
-        <button type="submit">Klik hier om te boeken!</button>
-      </form>
+      <div className="campeerplek">
+      <section className='customer-info'>
+      <div className="form-section left-section">
+        <form>
+          <div>
+            <label htmlFor="voornaam">Voornaam:</label>
+            <input
+              type="text"
+              id="voornaam"
+              name="voornaam"
+              value={formData.voornaam}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="tussenvoegsel">Tussenvoegsel:</label>
+            <input
+              type="text"
+              id="tussenvoegsel"
+              name="tussenvoegsel"
+              value={formData.tussenvoegsel}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="achternaam">Achternaam:</label>
+            <input
+              type="text"
+              id="achternaam"
+              name="achternaam"
+              value={formData.achternaam}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="telefoonnummer">Telefoonnummer:</label>
+            <input
+              type="tel"
+              id="telefoonnummer"
+              name="telefoonnummer"
+              value={formData.telefoonnummer}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+      </div>
+
+      <div className="form-section right-section">
+        <form>
+          <div className='right-side'>
+            <div>
+              <label htmlFor="volwassenen">Volwassene vanaf 16 jaar:</label>
+              <select
+                id="volwassenen"
+                name="volwassenen"
+                value={formData.volwassenen}
+                onChange={handleSelectChange}
+              >
+                {[...Array(10).keys()].map((number) => (
+                  <option key={number + 1} value={number + 1}>
+                    {number + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="tieners">Kinderen van 12 t/m 15 jaar:</label>
+              <select
+                id="tieners"
+                name="tieners"
+                value={formData.tieners}
+                onChange={handleSelectChange}
+              >
+                {[...Array(10).keys()].map((number) => (
+                  <option key={number} value={number}>
+                    {number}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="kinderen">Kinderen tot 11 jaar:</label>
+              <select
+                id="kinderen"
+                name="kinderen"
+                value={formData.kinderen}
+                onChange={handleSelectChange}
+              >
+                {[...Array(10).keys()].map((number) => (
+                  <option key={number} value={number}>
+                    {number}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </form>
+      </div>
+</section>   
+
+      <div className="form-section common-section">
+        <label htmlFor="accommodatieType">Welke type accommodatie wilt u huren:</label>
+        <select
+          id="accommodatieType"
+          name="accommodatieType"
+          value={formData.accommodatieType}
+          onChange={handleSelectChange}
+        >
+          <option value="Selecteer">Selecteer</option>
+          <option value="Stacaravan">Stacaravan</option>
+          <option value="Vakantiewoning">Vakantiewoning</option>
+          <option value="Kampeerplaats">Kampeerplaats</option>
+          <option value="Tent">Tent</option>
+        </select>
+      </div>
+
+      <Calender />
     </div>
+  </div>
   );
-}
+};
 
 export default BookingForm;
-  
