842667ecc0e90b79816cf0ecec92ec5504cc1b8c
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-12 13:32:12 +0100

    CTA verbeterd

--- BertsBoerenCamping/src/components/CallToAction.jsx
+++ BertsBoerenCamping/src/components/CallToAction.jsx
@@ -5,12 +5,12 @@ const CallToAction = () => {
     return(
         <>
         <div className="call-to-action">
-            <p1>Van boerderij naar camping </p1>
-            <h1>Van boerderij naar camping</h1>
-            <p>Een ontspanning voor alle leeftijden</p>
-            <button className="boeken-button">NU BOEKEN</button>
-            <img src="../images/CTA_Achtergrond.png" alt="Call To Action Achtergrond" className="cta-achtergrond"/>  
+            <h1 className="cta-title">Van boerderij naar camping</h1>
+            <p1 className="cta-title-cursive">Van boerderij naar camping </p1>
+            <p className="cta-beschrijving">Een ontspanning voor alle leeftijden</p>
+            <button className="boeken-button">NU BOEKEN</button>  
         </div>
+        <img src="../images/CTA_Achtergrond.png" alt="Call To Action Achtergrond" className="cta-achtergrond"/>  
         </>
     );
 };
