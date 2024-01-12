81c0cc77981ebc73c43f5b3adab425b46ed2dee2
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-12 19:29:57 +0100

    boeken info en faq css gefixt

--- BertsBoerenCamping/src/components/BoekenInfo.jsx
+++ BertsBoerenCamping/src/components/BoekenInfo.jsx
@@ -4,12 +4,12 @@ import './BoekenInfo.css';
 const BoekenInfo = () => {
     return(
         <>
-            <p3 className="hoe-moet-je-boeken-cursive">hoe moet je boeken</p3>
-            <h3 className="hoe-moet-je-boeken">HOE MOET JE BOEKEN</h3>
-            
-            <p3 className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus bibendum eros et lorem vestibulum consectetur. Proin eget libero ac lorem congue ullamcorper. Nullam in ullamcorper lectus. In id tortor molestie, mattis augue id, dapibus magna. Nam eros lorem, scelerisque eu nisl eu, sodales varius sapien. Fusce a diam turpis. Pellentesque et dapibus nibh. Nam at leo et ipsum dictum tristique. Phasellus ultrices velit iaculis, sollicitudin metus ac, euismod libero. Donec vitae euismod ex, non maximus lacus. Donec pharetra sapien ac lectus vehicula dictum. Vestibulum tincidunt sapien erat, vel vestibulum magna commodo ac.
-            <br/>
-            Sed pharetra porta neque ac venenatis. Aenean libero orci, aliquet a justo ac, ultricies ultricies eros. Etiam id tellus ac lectus venenatis pellentesque. Aenean at ultricies ex, at porttitor arcu. Etiam et auctor lacus. Sed interdum lectus diam, id sagittis risus volutpat sit amet. Phasellus bibendum consectetur aliquet. Nam non diam suscipit, convallis ipsum et,</p3>
+        <div className="hoe-moet-je-boeken">
+            <h3 className="hoe-moet-je-boeken-title">HOE MOET JE BOEKEN</h3>
+            <p3 className="hoe-moet-je-boeken-title-cursive">hoe moet je boeken</p3>
+
+            <p3 className="description">Het reserveren van een campingplek is niet ingewikkeld. Onze boekingspagina heeft een simpele, klikbare plattegrond. Deze plattegrond is voor het creëren van een visueel van onze camping. Naast deze plattegrond is er ook een legenda afgebeeld, waardoor u kunt zien welke kleuren er zijn en wat ze betekenen. De campingplekken die te reserveren zijn zijn gelabeld van A tot G. Als u op een campingplek of het donkergroene vlak klikt, dan kunt u een reservering aanmaken voor die gekozen campingplek. De informatie, zoals beschikbare dagen en foto’s zijn toegevoegd voor die campingplek en veranderen elke keer wanneer een nieuwe campingplek is geselecteerd. Als u vergeten bent hoe u moet reserveren, staat er nog een kleine uitleg op de boekingspagina zelf om u te helpen.</p3>
+        </div>
         </>
     );
 };
