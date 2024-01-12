aa2df8df7f3261ee82e4ba69a512017aac638b33
FlamingViperr <marinvanes12@gmail.com>
2024-01-12 09:30:48 +0100

    Poging tot Cookie met JWT
    Code werkt nog niet, deze commit is voor delen van informatie

--- BertsBoerenCamping/server.js
+++ BertsBoerenCamping/server.js
@@ -6,6 +6,12 @@ const mysql2 = require('mysql2');
 const bcrypt = require('bcrypt');
 const app = express();
 
+// -M
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+//
+
 const port = 3001;
 
 app.use(cors());
@@ -51,9 +57,9 @@ app.post('/api/register', async (req, res) => {
     }
   });
 });
-
+/*
 // Baliemedewerker login
-app.post('/api/login', (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
 
   db.query('SELECT * FROM baliemedewerker WHERE email = ?', [email], async (err, result) => {
@@ -74,7 +80,14 @@ app.post('/api/login', (req, res) => {
           // Hier ben ik mee bezig - Niek
           // db.query('SELECT * FROM baliemedewerker WHERE rol = ?', [rol], (err, result) => {
           // })
-
+          
+          // -M
+          const user = { emailid: email }
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+          res.cookie('jwt', accessToken, {httpOnly: true, sameSite: 'None', secure: true});
+          
+          console.log('cookie set:', accessToken);
+          //
 
         } else {
           res.status(401).json({ error: 'Invalid password' });
@@ -84,7 +97,38 @@ app.post('/api/login', (req, res) => {
       }
     }
   });
+});*/
+
+
+// -M
+// Baliemedewerker Login
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // const result = await db.QueryPromise('SELECT * FROM baliemedewerker WHERE email = ?', [email]);
+    const result = db.query('SELECT * FROM baliemedewerker WHERE email = ?', [email]);
+
+    if (result.length > 0) {
+      const match = await bcrypt.compare(password, result[0].wachtwoord);
+      if (match) {
+        const user = { emailid: email }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+        res.cookie('jwt', accessToken, { httpOnly: true, sameSite: 'None', secure: true });
+        res.status(200).json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ error: 'Invalid password' });
+      }
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (err) {
+    console.error('Error retrieving user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+//
+
 
 
 // Invoeren gast gegevens
