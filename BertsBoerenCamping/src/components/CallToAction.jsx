940655460e017037a00484af9ee2e3e424a2db9b
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-16 11:26:02 +0100

    CTA Responsive

--- BertsBoerenCamping/src/components/CallToAction.jsx
+++ BertsBoerenCamping/src/components/CallToAction.jsx
@@ -9,7 +9,8 @@ const CallToAction = () => {
             <h1>Van boerderij naar camping</h1>
             <p>Een ontspanning voor alle leeftijden</p>
             <button className="boeken-button">NU BOEKEN</button>
-            <img src="../images/CTA_Achtergrond.png" alt="Call To Action Achtergrond" className="cta-achtergrond"/>  
+            <img src="../images/CTA_Achtergrond.png" alt="Call To Action Achtergrond" className="cta-achtergrond"/> 
+            <img src="../images/CTA_Achtergrond_Responsive.png" alt="Call To Action" className="cta-achtergrond-responsive"/>
         </div>
         </>
     );
