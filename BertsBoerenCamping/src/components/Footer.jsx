41c74760e5e0d9d58b80a7aa397530d5013a4948
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-17 10:31:44 +0100

    Footer een klein beetje gefixt

--- BertsBoerenCamping/src/components/Footer.jsx
+++ BertsBoerenCamping/src/components/Footer.jsx
@@ -9,21 +9,21 @@ const Footer = () => {
             <div className="footer">
                 <div className="footer-heading-1">
                     <h6 className="heading">BOEKEN</h6>
-                    <Link to="/booking">Boeken</Link>
-                    <Link to="/booking">Reserveren</Link>
+                    <Link to="/booking" className="link">Boeken</Link>
+                    <Link to="/booking" className="link">Reserveren</Link>
                 </div>
 
                 <div className="footer-heading-2">
                     <h6 className="heading">OVER ONS</h6>
-                    <Link to="/about">Over Ons</Link>
+                    <Link to="/about" className="link">Over Ons</Link>
                 </div>
 
                 <div className="footer-heading-3">
                     <h6 className="heading">CONTACT</h6>
-                    <a href="#">Adres</a>
-                    <a href="#">info@example.com</a>
-                    <a href="#">+01 234 567 89</a>
-                    <a href="#">+01 234 567 89</a>
+                    <a href="#" className="link">Adres</a>
+                    <a href="#" className="link">info@example.com</a>
+                    <a href="#" className="link">+01 234 567 89</a>
+                    <a href="#" className="link">+01 234 567 89</a>
                     <br/>
                     <label for="footer-email">Maandelijkse nieuwsbrief</label>
                     <input type="email" placeholder="Email" id="footer-email"/>
