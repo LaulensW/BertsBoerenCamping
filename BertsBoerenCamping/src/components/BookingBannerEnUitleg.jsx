5ce07c7976383400542d49e81c1aa01b1aed1aaf
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-19 09:40:12 +0100

    Plattegrond een beetje aangepast en tekst veranderd

--- BertsBoerenCamping/src/components/BookingBannerEnUitleg.jsx
+++ BertsBoerenCamping/src/components/BookingBannerEnUitleg.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
         <div className="boeken-uitleg">
         <h3 className="hoe-moet-je-boeken-title">HOE MOET JE BOEKEN</h3>
         <p3 className="hoe-moet-je-boeken-title-cursive">hoe moet je boeken</p3>
-        <p3 className="boeken-uitleg-tekst">Onze boekingspagina heeft een simpele, klikbare plattegrond. Deze plattegrond is voor het creëren van een visueel van onze camping. Naast deze plattegrond is er ook een legenda afgebeeld, waardoor u kunt zien welke kleuren er zijn en wat ze betekenen. De campingplekken die te reserveren zijn zijn gelabeld van A tot G. Als u op een campingplek of het donkergroene vlak klikt, dan kunt u een reservering aanmaken voor die gekozen campingplek. De informatie, zoals beschikbare dagen en foto’s zijn toegevoegd voor die campingplek en veranderen elke keer wanneer een nieuwe campingplek is geselecteerd.</p3>
+        <p3 className="boeken-uitleg-tekst">Onze boekingspagina heeft een simpele plattegrond. Deze plattegrond is voor het creëren van een visueel van onze camping. Naast deze plattegrond is er ook een legenda afgebeeld, waardoor u kunt zien welke kleuren er zijn en wat ze betekenen. De campingplekken die te reserveren zijn zijn gelabeld van A tot G. Onder de plattegrond staat het reserveren. Het enige wat u hierbij hoeft in te vullen zijn uw persoonlijke gegevens, zoals uw naam, email telefoonnummer etc. Daarnaast kunt uw ook invullen welk soort accommodatie uw wilt huren en op welke plek uw dat wilt. Als laatst staat er een kalender met data waar uw uit kunt kiezen. </p3>
         </div>
         </>
     );
---
+++ BertsBoerenCamping/src/components/Reviews.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Reviews.css';
+import ReviewsWave from '../assets/reviews-wave.svg';
+
+
+export default function Reviews() {
+    return (
+        <>
+           <img src={ReviewsWave}  alt='reviewswave' className='reviewswave'/>
+            <h3 className="reviews">REVIEWS</h3>
+            <p3 className="reviews-cursive">reviews</p3>
+            <div className="reviews-total">
+                {renderReview("Review Title 1", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+                {renderReview("Review Title 2", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+                {renderReview("Review Title 3", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+                {renderReview("Review Title 4", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+                {renderReview("Review Title 5", "Author", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc metus libero, dignissim et arcu id, consequat rutrum leo. Morbi pretium nec lorem vel porta. Aenean finibus faucibus commodo. Vivamus quis justo neque. Sed et pulvinar magna. Duis nec laoreet purus. Vivamus tempus leo vitae sem vestibulum volutpat.")}
+            </div>
+        </>
+    );
+};
+
+function renderReview (title, author, content) {
+
+    return (
+       <div className="reviewcard" key={title}>
+            <div className="review-info">
+                <h4 className="review-title">{title}</h4>
+                <p4 className="review-author">{author}</p4>
+                <p3 className="review-content">{content}</p3>
+            </div>
+       </div>
+    );
+};
+
