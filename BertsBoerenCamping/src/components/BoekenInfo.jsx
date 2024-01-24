8431664e1923c09a4d1a125eb30c766773a193a2
robinnguyenx <149425050+robinnguyenx@users.noreply.github.com>
2024-01-24 17:47:09 +0100

    kleine foutjes gefixt

--- BertsBoerenCamping/src/components/BoekenInfo.jsx
+++ BertsBoerenCamping/src/components/BoekenInfo.jsx
@@ -7,7 +7,6 @@ const BoekenInfo = () => {
         <div className="hoe-moet-je-boeken">
             <h3 className="hoe-moet-je-boeken-title">HOE MOET JE BOEKEN</h3>
             <p3 className="hoe-moet-je-boeken-title-cursive">hoe moet je boeken</p3>
-
             <p3 className="description">Het reserveren van een campingplek is niet ingewikkeld. Onze boekingspagina heeft een simpele, klikbare plattegrond. Deze plattegrond is voor het creëren van een visueel van onze camping. Naast deze plattegrond is er ook een legenda afgebeeld, waardoor u kunt zien welke kleuren er zijn en wat ze betekenen. De campingplekken die te reserveren zijn zijn gelabeld van A tot G. Als u op een campingplek of het donkergroene vlak klikt, dan kunt u een reservering aanmaken voor die gekozen campingplek. De informatie, zoals beschikbare dagen en foto’s zijn toegevoegd voor die campingplek en veranderen elke keer wanneer een nieuwe campingplek is geselecteerd. Als u vergeten bent hoe u moet reserveren, staat er nog een kleine uitleg op de boekingspagina zelf om u te helpen.</p3>
         </div>
         </>
